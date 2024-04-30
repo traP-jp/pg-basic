522b161753acdd03b4b9370df577c3ac845455be
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2024-04-30 11:46:56 +0900

    hello-shell.zip

---
+++ docs/text/chapter-1/practice/hello-shell/generator.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+generate_folder_name() {
+    chars='abcdef0123456789'
+    folder_name=$(cat /dev/urandom | LC_CTYPE=C tr -dc "$chars" | fold -w 10 | head -n 1)
+    echo "$folder_name"
+}
+
+current_directory="$(pwd)"
+
+# Create 20 folders
+for ((i=1; i<=20; i++)); do
+    # Generate folder name
+    folder_name=$(generate_folder_name)
+    # Create folder
+    mkdir "$folder_name"
+    echo "Created folder: $folder_name"
+    cd "$folder_name"
+    for ((j=1; j<=20; j++)); do
+        f2_name=$(generate_folder_name)
+        mkdir "$f2_name"
+        echo "Created folder: $folder_name/$f2_name"
+    done
+
+    cd "$run_directory"
+    
+done
